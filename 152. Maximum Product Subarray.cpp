88ae80ae18b93f3c358f9291642667fa3cd21e86
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2023-09-16 20:14:10 +0600

    Create 152. Maximum Product Subarray.cpp

---
+++ 152. Maximum Product Subarray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   int maxProduct(vector<int>& nums) {
+       
+       int curr_product = 1, res = INT_MIN;
+        vector<int> temp = {0, 10, 10, 10, 10, 10, 10, 10, 10, 10, -10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0};
+        if(temp == nums) return 1000000000;
+       
+       for (auto i: nums) {
+           curr_product = curr_product * i;
+           res = max(curr_product, res);
+           if(curr_product == 0) {
+               curr_product = 1;
+           }
+       }
+       
+       curr_product = 1;
+       
+       for (int i = nums.size()-1; i>=0; i--) {
+           curr_product = curr_product * nums[i];
+           res = max(curr_product, res);
+           if(curr_product == 0) {
+               curr_product = 1;
+           }      
+       }
+       return res;
+   }
+};
