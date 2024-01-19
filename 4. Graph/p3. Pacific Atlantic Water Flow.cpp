b291cabef148e9c1f229287166d40c860e8092e3
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-01-19 18:45:33 +0600

    Update p3. Pacific Atlantic Water Flow.cpp

--- 4. Graph/p3. Pacific Atlantic Water Flow.cpp
+++ 4. Graph/p3. Pacific Atlantic Water Flow.cpp
@@ -1,4 +1,5 @@
-// Tree BFS
+1. 
+// Tree BFS (Unoptimized)
 
 class Solution {
 public:
@@ -76,3 +77,80 @@ public:
         return ans;
     }
 };
+
+
+2. Tree BFS (optimized)
+// https://www.youtube.com/watch?v=s-VkcjHqkGI&ab_channel=NeetCode
+    
+class Solution {
+public:
+    int n, m;
+    
+    bool inLimit(int r, int c) {
+        return ((r >= 0 && r < n) && (c >= 0 && c < m));
+    }
+    
+    void bfs(int r, int c, vector<vector<int>>& heights, int visited[][201]) {
+        int h, i, j, newX, newY, newH;
+        visited[r][c] = 1;
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        
+        while (!q.empty()) {
+            r = q.front().first;
+            c = q.front().second;
+            h = heights[r][c];
+            q.pop();
+            
+            int x[4] = {-1, 0, 1, 0};
+            int y[4] = {0, 1, 0, -1};
+            
+            for (i = 0; i < 4; i++) {
+                newX = r + x[i];
+                newY = c + y[i];
+                
+                if (inLimit(newX, newY) && h <= heights[newX][newY] && visited[newX][newY] == 0) {
+                    visited[newX][newY] = true;
+                    q.push({newX, newY});
+                }
+            }   
+        }
+    }
+    
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int i, j;
+        vector<vector<int>> ans;
+        n = heights.size();
+        m = heights[0].size();
+        
+        int pacific[201][201];
+        int atlantic[201][201];
+        
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                pacific[i][j] = 0;
+                atlantic[i][j] = 0;
+            }
+        }
+        
+        for (i = 0; i < m; i++) {
+            bfs(0, i, heights, pacific);
+            bfs(n-1, i, heights, atlantic);
+        }
+        
+        for (i = 0; i < n; i++) {
+            bfs(i, 0, heights, pacific);
+            bfs(i, m-1, heights, atlantic);
+        }
+        
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                if (pacific[i][j] == 1 && atlantic[i][j] == 1)
+                    ans.push_back({i, j});
+            }
+        }
+        
+        return ans;
+    }
+};
+    
