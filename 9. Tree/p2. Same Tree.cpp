bd84005d09d1aea5afaa521fbfb0329ca4a0f787
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-01-20 19:04:14 +0600

    Create p2. Same Tree.cpp

---
+++ 9. Tree/p2. Same Tree.cpp
@@ -0,0 +1,53 @@
+// Tree BFS
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL)
+            return true;
+        else if (p == NULL || q == NULL)
+            return false;
+        
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        q1.push(p);
+        q2.push(q);
+        
+        while (!q1.empty() && !q2.empty()) {
+            TreeNode* cur1 = q1.front();
+            TreeNode* cur2 = q2.front();
+            q1.pop();
+            q2.pop();
+            
+            if (cur1->val != cur2->val)
+                return false;
+            
+            if ((cur1->left == NULL && cur2->left != NULL) || (cur1->left != NULL && cur2->left == NULL))
+                return false;
+            else if ((cur1->right == NULL && cur2->right != NULL) || (cur1->right != NULL && cur2->right == NULL)) 
+                return false;
+            
+            if ((cur1->right != NULL && cur2->right != NULL)) {
+                q1.push(cur1->right);
+                q2.push(cur2->right);
+            }
+            
+            if ((cur1->left != NULL && cur2->left != NULL)) {
+                q1.push(cur1->left);
+                q2.push(cur2->left);
+            }
+        }
+        return true;
+    }
+};
