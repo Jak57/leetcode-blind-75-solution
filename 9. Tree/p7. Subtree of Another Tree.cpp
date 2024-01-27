19caedb56b32516d0791a60ecde35a42deea34a9
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-01-27 08:04:14 +0600

    Update p7. Subtree of Another Tree.cpp

--- 9. Tree/p7. Subtree of Another Tree.cpp
+++ 9. Tree/p7. Subtree of Another Tree.cpp
@@ -62,3 +62,46 @@ public:
         return false;
     }
 };
+
+
+// 2.
+// Tree DFS
+// https://www.youtube.com/watch?v=E36O5SWp-LE&ab_channel=NeetCode
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL)
+            return true;
+        else if (p == NULL || q == NULL)
+            return false;
+        else
+            return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == NULL && subRoot == NULL)
+            return true;
+        else if (subRoot == NULL)
+            return true;
+        else if (root == NULL)
+            return false;
+        else {
+            if (isSameTree(root, subRoot))
+                return true;
+            else
+                return (isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot));
+        }
+    }
+};
