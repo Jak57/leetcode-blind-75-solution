8a11ca497617a8af630bad433ff5f6c90acb12cf
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-02-01 15:44:13 +0600

    Update p7. Valid Palindrome.cpp

--- 8. String/p7. Valid Palindrome.cpp
+++ 8. String/p7. Valid Palindrome.cpp
@@ -1,3 +1,5 @@
+// 1.
+
 class Solution {
 public:
     bool isPalindrome(string s) {
@@ -26,6 +28,46 @@ public:
             j--;
         }
         
+        return true;
+    }
+};
+
+// 2.
+
+class Solution {
+public:
+    bool isValid(char ch) {
+        return ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'));
+    }
+    
+    bool isPalindrome(string s) {
+        int n, i, j;
+        char ch1, ch2;
+        n = s.size();
+
+        for (i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch >= 'A' && ch <= 'Z')
+                s[i] = s[i] - 'A' + 'a';
+        }
+        
+        i = 0;
+        j = n-1;
+        while (i < j) {
+            ch1 = s[i];
+            ch2 = s[j];
+            if (!isValid(ch1))
+                i++;
+            if (!isValid(ch2))
+                j--;
+            
+            if (isValid(ch1) && isValid(ch2)) {
+                if (ch1 != ch2)
+                    return false;
+                i++;
+                j--;
+            }
+        }
         return true;
     }
 };
