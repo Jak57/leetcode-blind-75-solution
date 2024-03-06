28c6234310a8152c00375063de75fb3e05443422
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-03-06 18:43:07 +0600

    Create 12. Implement Trie (Prefix Tree).cpp

---
+++ 9. Tree/12. Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,75 @@
+// https://www.geeksforgeeks.org/trie-insert-and-search/
+
+class Trie {
+public:
+    struct TrieNode{
+        TrieNode* child[26];
+        bool isEnd;
+    };
+    
+    TrieNode* getNode() {
+        TrieNode* node = new TrieNode;
+        int i;
+        for (i = 0; i < 26; i++) {
+            node->child[i] = NULL;
+            node->isEnd = false;
+        }
+        return node;
+    }
+    
+    TrieNode* root;
+    Trie() {
+        root = getNode();
+    }
+    
+    void insert(string word) {
+        int i, idx, n;
+        n = word.size();
+        TrieNode* cur = root;
+        
+        for (i = 0; i < n; i++) {
+            idx = word[i] - 'a';
+            if (cur->child[idx] == NULL)
+                cur->child[idx] = getNode();
+            cur = cur->child[idx];
+        }
+        cur->isEnd = true;
+    }
+    
+    bool search(string word) {
+        int i, idx, n;
+        n = word.size();
+        TrieNode* cur = root;
+        
+        for (i = 0; i < n; i++) {
+            idx = word[i] - 'a';
+            if (cur->child[idx] == NULL)
+                return false;
+            cur = cur->child[idx];
+        }
+        return cur->isEnd;
+    }
+    
+    bool startsWith(string prefix) {
+        int i, idx, n;
+        string word = prefix;
+        n = word.size();
+        TrieNode* cur = root;
+        
+        for (i = 0; i < n; i++) {
+            idx = word[i] - 'a';
+            if (cur->child[idx] == NULL)
+                return false;
+            cur = cur->child[idx];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
